5cd9d666ca6d070792013eb3367e6f4205f840f5
raj5253 <rajsatyam8532@yahoo.com>
2024-04-15 23:31:50 +0530

    four3

--- app/(dashboard)/crop_recommend/page.jsx
+++ app/(dashboard)/crop_recommend/page.jsx
@@ -2,3 +2,13 @@
  * We will have one form where user input The soil conditions on according to Dutch will protect the best crop.
  * 
  */
+
+import React from 'react'
+
+const page = () => {
+    return (
+        <div>page</div>
+    )
+}
+
+export default page
--- app/(dashboard)/monitor/page.tsx
+++ app/(dashboard)/monitor/page.tsx
@@ -1,1 +1,9 @@
 //display all the crop which user(farmer) has added
+
+import React from "react";
+
+const page = () => {
+  return <div>page</div>;
+};
+
+export default page;
