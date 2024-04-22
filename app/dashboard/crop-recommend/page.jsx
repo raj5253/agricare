ed7bd6a66813be20197549bc8888e3f50656d012
Rahul <sahrahul493@gmail.com>
2024-04-22 22:08:53 +0530

    updated

--- app/dashboard/crop-recommend/page.jsx
+++ app/dashboard/crop-recommend/page.jsx
@@ -1,120 +1,211 @@
-"use client"
-import { useState } from "react"
+"use client";
+import { useState } from "react";
 // import axiosInstance from "@/axiosInstance"
-import axios from "axios"
+import axios from "axios";
 
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { useRouter } from "next/navigation"
-import Link from "next/link"
-
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 export default function ProfileForm() {
-    const router = useRouter();
+  const router = useRouter();
 
-    const [loading, setLoading] = useState(false);
-    const [errMssg, setErrMssg] = useState('')
-    const [N, setN] = useState('')
-    const [K, setK] = useState('')
-    const [P, setP] = useState('')
-    const [rainfall, setRainfall] = useState('')
-    const [humidity, setHumidity] = useState('')
-    const [temperature, setTemperature] = useState('')
-    const [pH, setpH] = useState('')
-    const [crop, setCrop] = useState('')//initailly must be empty
+  const [loading, setLoading] = useState(false);
+  const [errMssg, setErrMssg] = useState("");
+  const [N, setN] = useState();
+  const [K, setK] = useState();
+  const [P, setP] = useState();
+  const [rainfall, setRainfall] = useState();
+  const [humidity, setHumidity] = useState();
+  const [temperature, setTemperature] = useState();
+  const [pH, setpH] = useState();
+  const [crop, setCrop] = useState(); //initailly must be empty
 
+  async function handleSubmit(e) {
+    e.preventDefault();
 
-    async function handleSubmit(e) {
-        e.preventDefault();
-
-        if (!N || !K || !P || !rainfall || !humidity || !pH || !temperature) {
-            setErrMssg("Please Enter all fields correctly!")
-            console.log(" call returuning")
-            return;
+    if (!N || !K || !P || !rainfall || !humidity || !pH || !temperature) {
+      setErrMssg("Please Enter all fields correctly!");
+      console.log(" call returuning");
+      return;
+    } else {
+      setLoading(true);
+      try {
+        const data = {
+          N: Number(N),
+          P: Number(P),
+          K: Number(K),
+          temperature: Number(temperature),
+          humidity: Number(humidity),
+          pH: Number(pH),
+          rainfall: Number(rainfall),
+        };
+        console.log(data);
+        const res = await axios.post(
+          "http://127.0.0.1:5000/api/crop_recommender",
+          data
+        );
+        if (res.status === 200) {
+          const data = res.data;
+          setCrop(data?.prediction);
         }
-        else {
-            setLoading(true);
-            try {
-                const data = { N, P, K, temperature, humidity, pH, rainfall }
-                console.log(data);
-                // const res = await axios.post("/py/crop-recommend", data);
-                // if (res.status === 200) {
-                //     //rahul 
-                //     console.log(res.data)
-                setCrop('Rice')
-                // }
-            } catch (error) {
-                console.error("Error:", error);
-            } finally {
-                setLoading(false);
-            }
-        }
+      } catch (error) {
+        console.error("Error:", error);
+      } finally {
+        setLoading(false);
+      }
     }
+  }
 
-    return (
-        <div className="container" >
-            <h1 className="h5-bold ml-8">Best Crop for your land</h1>
-            <p className="ml-8 mt-1 mb-8 sm:mb-4 p-regular-14  text-muted-foreground">use our best crop recommendation model to find the best suitable crop for your land</p>
-            <section >
-                <form onSubmit={handleSubmit} className="space-y-8 mx-auto md:max-w-md px-4">
-                    <section className="flex gap-3 justify-evenly ">
-                        <div className="space-y-2">
-                            <label>Nitrogen(N)</label>
-                            <Input placeholder="Nitrogen" value={N} onChange={(e) => { setN(e.target.value); setErrMssg('') }} type="number" />
-                        </div>
-                        <div className="space-y-2">
-                            <label>Phosphorous(P)</label>
-                            <Input placeholder="Phosphorous" value={P} onChange={(e) => { setP(e.target.value); setErrMssg('') }} type="number" />
-                        </div>
-                        <div className="space-y-2">
-                            <label>Potassium(K)</label>
-                            <Input placeholder="Potassium" value={K} onChange={(e) => { setK(e.target.value); setErrMssg('') }} type="number" />
-                        </div>
-                    </section>
-                    <section className="flex flex-1 gap-3 justify-evenly ">
-                        <div className="space-y-2">
-                            <label>Temperature</label>
-                            <Input placeholder="temperature in °C" value={temperature} onChange={(e) => { setTemperature(e.target.value); setErrMssg('') }} type="number" />
-                        </div>
-                        <div className="space-y-2">
-                            <label>Humidity</label>
-                            <Input placeholder="Humidity in  g/m3" value={humidity} onChange={(e) => { setHumidity(e.target.value); setErrMssg('') }} type="number" />
-                        </div>
-                    </section>
-                    <section className="flex flex-1 gap-3 justify-evenly ">
-                        <div className="space-y-2">
-                            <label>pH of soil</label>
-                            <Input placeholder="0 - 14" value={pH} onChange={(e) => { setpH(e.target.value); setErrMssg('') }} type="number" />
-                        </div>
-                        <div className="space-y-2">
-                            <label>Rainfall</label>
-                            <Input placeholder="Rainfall in mm" value={rainfall} onChange={(e) => { setRainfall(e.target.value); setErrMssg('') }} type="number" />
-                        </div>
-                    </section>
-                    <Button type="submit" variant="default" disabled={loading} className="text-white" >Submit</Button>
-                </form>
-                <div>
-                    <p className="text-center text-xs text-destructive ">{errMssg}</p>
-                </div>
-            </section>
+  return (
+    <div className="container">
+      <h1 className="h5-bold ml-8">Best Crop for your land</h1>
+      <p className="ml-8 mt-1 mb-8 sm:mb-4 p-regular-14  text-muted-foreground">
+        use our best crop recommendation model to find the best suitable crop
+        for your land
+      </p>
+      <section>
+        <form
+          onSubmit={handleSubmit}
+          className="space-y-8 mx-auto md:max-w-md px-4"
+        >
+          <section className="flex gap-3 justify-evenly ">
+            <div className="space-y-2">
+              <label>Nitrogen(N)</label>
+              <Input
+                placeholder="Nitrogen"
+                value={N}
+                onChange={(e) => {
+                  setN(e.target.value);
+                  setErrMssg("");
+                }}
+                type="number"
+              />
+            </div>
+            <div className="space-y-2">
+              <label>Phosphorous(P)</label>
+              <Input
+                placeholder="Phosphorous"
+                value={P}
+                onChange={(e) => {
+                  setP(e.target.value);
+                  setErrMssg("");
+                }}
+                type="number"
+              />
+            </div>
+            <div className="space-y-2">
+              <label>Potassium(K)</label>
+              <Input
+                placeholder="Potassium"
+                value={K}
+                onChange={(e) => {
+                  setK(e.target.value);
+                  setErrMssg("");
+                }}
+                type="number"
+              />
+            </div>
+          </section>
+          <section className="flex flex-1 gap-3 justify-evenly ">
+            <div className="space-y-2">
+              <label>Temperature</label>
+              <Input
+                placeholder="temperature in °C"
+                value={temperature}
+                onChange={(e) => {
+                  setTemperature(e.target.value);
+                  setErrMssg("");
+                }}
+                type="number"
+              />
+            </div>
+            <div className="space-y-2">
+              <label>Humidity</label>
+              <Input
+                placeholder="Humidity in  g/m3"
+                value={humidity}
+                onChange={(e) => {
+                  setHumidity(e.target.value);
+                  setErrMssg("");
+                }}
+                type="number"
+              />
+            </div>
+          </section>
+          <section className="flex flex-1 gap-3 justify-evenly ">
+            <div className="space-y-2">
+              <label>pH of soil</label>
+              <Input
+                placeholder="0 - 14"
+                value={pH}
+                onChange={(e) => {
+                  setpH(e.target.value);
+                  setErrMssg("");
+                }}
+                type="number"
+              />
+            </div>
+            <div className="space-y-2">
+              <label>Rainfall</label>
+              <Input
+                placeholder="Rainfall in mm"
+                value={rainfall}
+                onChange={(e) => {
+                  setRainfall(e.target.value);
+                  setErrMssg("");
+                }}
+                type="number"
+              />
+            </div>
+          </section>
+          <Button
+            type="submit"
+            variant="default"
+            disabled={loading}
+            className="text-white"
+          >
+            Submit
+          </Button>
+        </form>
+        <div>
+          <p className="text-center text-xs text-destructive ">{errMssg}</p>
+        </div>
+      </section>
 
-            {crop &&
-                <section>
-                    <p className="p-regular-14 mt-8">The best suitable crop to your land is  <span className="italic">{crop}</span></p>
-                    <p className="p-regular-14 mt-2"> Want to monitor the crop ? <Link href={{
-                        pathname: "/dashboard/add-crop",
-                        query: { N, P, K, pH, crop }
-                    }}><Button variant="linkp" className="font-bold"
-                    // onClick={(e) => {
-                    // e.preventDefault(); router.push({
-                    //     pathname: "/dashboard/add-crop",
-                    //     query: { N, P, K, pH, crop }
-                    // });
-                    // }}
-                    > Add Crop</Button></Link> </p>
-                </section>
-            }
-        </div >
-    )
+      {crop && (
+        <section>
+          <p className="p-regular-14 mt-8">
+            The best suitable crop to your land is{" "}
+            <span className="italic">{crop}</span>
+          </p>
+          <p className="p-regular-14 mt-2">
+            {" "}
+            Want to monitor the crop ?{" "}
+            <Link
+              href={{
+                pathname: "/dashboard/add-crop",
+                query: { N, P, K, pH, crop },
+              }}
+            >
+              <Button
+                variant="linkp"
+                className="font-bold"
+                // onClick={(e) => {
+                // e.preventDefault(); router.push({
+                //     pathname: "/dashboard/add-crop",
+                //     query: { N, P, K, pH, crop }
+                // });
+                // }}
+              >
+                {" "}
+                Add Crop
+              </Button>
+            </Link>{" "}
+          </p>
+        </section>
+      )}
+    </div>
+  );
 }
-
--- app/dashboard/monitor/[cropId]/page.jsx
+++ app/dashboard/monitor/[cropId]/page.jsx
@@ -1,44 +1,106 @@
-'use client'
+"use client";
 
 import React, { useEffect, useState } from "react";
-import { useParams } from 'next/navigation'
+import { useParams } from "next/navigation";
 import axios from "axios";
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
 
 const CropDetailPage = () => {
-  const [crop, setCrop] = useState(null)
+  const [crop, setCrop] = useState(null);
   const params = useParams(); //there are other hooks also: userSearchParams, useQueryParams
+  const [isLoading, setIsLoading] = useState(false);
+  const [endDate, setEndDate] = useState();
+  const [water, setWater] = useState(0);
 
   useEffect(() => {
     const preFetch = async () => {
       const res = await axios.get(`/api/user/crops/${params.cropId}`);
       if (res.status === 200) {
-        setCrop(res.data.crop)
-        console.log(res.data)
+        setCrop(res.data.crop);
+        const cropdata = res.data.crop;
+        const endDate = new Date(cropdata?.startDate);
+        endDate.setDate(endDate.getDate() + cropdata?.period);
+        let w = 0;
+        cropdata?.water.forEach((et) => {
+          w += et;
+        });
+        setWater(Math.round((w * cropdata?.area) / (1000 * 0.6)));
+        setEndDate(endDate);
+        console.log(res.data);
       }
+    };
+
+    preFetch();
+    return () => {};
+  }, [params.cropId, isLoading]);
+
+  const clickhandler = async () => {
+    try {
+      setIsLoading(true);
+      const res = await axios.get(
+        `http://127.0.0.1:5000/api/crop_details/${params.cropId}/update`
+      );
+      if (res.status === 200) {
+        toast.success("Updated");
+        console.log(res.data);
+      }
+    } catch (error) {
+      setIsLoading(false);
+      console.error(`Error : ${params.cropId} : `, error);
+    } finally {
+      setIsLoading(false);
     }
+  };
 
-    preFetch()
-    return () => { }
-  }, [params.cropId])
-
-  return <div >
-    <div className="container">
-      {crop && (
-        <div className="bg-white shadow-md rounded-lg p-4">
-          <h2 className="text-xl font-semibold mb-2">{crop.crop}</h2>
-          <p className="text-gray-600 mb-1">Start Date: {new Date(crop.startDate).toLocaleDateString()}</p>
-          <p className="text-gray-600 mb-1">Last Update: {new Date(crop.lastUpdate).toLocaleDateString()}</p>
-          <p className="text-gray-600 mb-1">End Date: {crop.endDate ? new Date(crop.endDate).toLocaleDateString() : 'N/A'}</p>
-          <p className="text-gray-600 mb-1">Location: {crop.location.latitude}, {crop.location.longitude}</p>
-          <p className="text-gray-600 mb-1">Area: {crop.area}</p>
-          <p className="text-gray-600 mb-1">Period: {crop.period}</p>
-          <p className="text-gray-600 mb-1">E Yield: {crop.e_yield || 'N/A'}</p>
-          <p className="text-gray-600 mb-1">Yield: {crop.yield || 'N/A'}</p>
-          <p className="text-gray-600 mb-1">Harvested: {crop.harvested ? 'Yes' : 'No'}</p>
-        </div>
-      )}
+  return (
+    <div>
+      <div className="container">
+        {crop && (
+          <div className="bg-white shadow-md rounded-lg p-4 relative">
+            <div className="absolute top-4 right-6">
+              <button onClick={clickhandler} disabled={isLoading}>
+                <img
+                  src={"/images/refresh.svg"}
+                  className="h-6 w-6"
+                  alt="refresh"
+                />
+              </button>
+            </div>
+            <h2 className="text-xl font-semibold mb-2">{crop.crop}</h2>
+            <p className="text-gray-600 mb-1">
+              Start Date: {new Date(crop.startDate).toLocaleDateString()}
+            </p>
+            <p className="text-gray-600 mb-1">
+              Last Update: {new Date(crop.lastUpdate).toLocaleDateString()}
+            </p>
+            <p className="text-gray-600 mb-1">
+              End Date: {endDate ? endDate.toLocaleDateString() : "N/A"}
+            </p>
+            <p className="text-gray-600 mb-1">
+              Location: {crop.location.latitude}, {crop.location.longitude}
+            </p>
+            <p className="text-gray-600 mb-1">
+              Area: {crop.area} m<sup>2</sup>
+            </p>
+            <p className="text-gray-600 mb-1">Period: {crop.period} days</p>
+            {/* <p className="text-gray-600 mb-1">
+              E Yield: {crop.e_yield || "N/A"}
+            </p>
+            <p className="text-gray-600 mb-1">Yield: {crop.yield || "N/A"}</p> */}
+            <p className="text-gray-600 mb-1">
+              Harvested: {crop.harvested ? "Yes" : "No"}
+            </p>
+            <p className="text-gray-600 mb-1">Water Required : {crop?.harvested ? "N/A" : <>{water} m<sup>3</sup></>}</p>
+            <div className="w-full flex flex-row items-center justify-center ">
+              {" "}
+              <Button> Update </Button>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
-  </div>
+  );
 };
 
-export default CropDetailPage
+export default CropDetailPage;
